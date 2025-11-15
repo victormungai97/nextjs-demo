87552e9937a5675deab8384bf7451047ba51893b
Victor Kamau <victormungai97@gmail.com>
2025-11-16 00:39:02 +0300

    Added dynamic metadata generation in pages

--- src/app/(dashboard)/dashboard/users/[id]/page.tsx
+++ src/app/(dashboard)/dashboard/users/[id]/page.tsx
@@ -1,3 +1,5 @@
+import { Metadata } from "next";
+
 // Shape of User Details
 interface UserDetailsProps {
   id: string;
@@ -7,6 +9,26 @@ interface UserDetailsProps {
 interface UserDetailsPageParams {
   params: Promise<UserDetailsProps>;
 }
+
+export const generateMetadata = async ({
+  params,
+}: UserDetailsPageParams): Promise<Metadata> => {
+  const { id } = await params;
+
+  return {
+    title: `User ${id} | NextJS Demo`,
+    description: "This page shows an individual user's details",
+    openGraph: {
+      type: "profile",
+      title: `User ${id} | NextJS Demo`,
+      description: "This page shows an individual user's details",
+    },
+    twitter: {
+      title: `User ${id} | NextJS Demo`,
+      description: "This page shows an individual user's details",
+    },
+  };
+};
 
 const UserDetails = async ({ params }: UserDetailsPageParams) => {
   const { id } = await params;
